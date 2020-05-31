f74318ec01c615883444a9aca6b2469be2fbe6b8
Pucha72 <dpushparaj72@gmail.com>
2020-05-31 20:29:09 +0530

    Revert "Revert "Initial commit""
    This reverts commit d469610df067a54b22407ab479c0dfeae3f677f5.

---
+++ LocalContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Models;
+namespace EF_Migration
+{
+    public class LocalContext : DbContext
+    {
+        public virtual DbSet<Student> student { get; set; }
+        public virtual DbSet<Standard> standard { get; set; }
+        public LocalContext() : base(ConfigurationSettings.AppSettings.Get("DatabaseLink").ToString())
+        // public LocalContext() : base("name=SchoolDBConnectionString")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<LocalContext, DBMigration>());
+        } 
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
